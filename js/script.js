9ea1d820378cb23bb54730ba105f9128b318acce
husnipicors <husnipicors10@gmail.com>
2022-01-28 11:34:23 +0700

    first commit

---
+++ js/script.js
@@ -0,0 +1,7 @@
+/* ============== Typing Animation ============ */
+var typed = new Typed(".typing", {
+  strings: ["", "Web Developer", "Back End Developer", "UI/UX Designer", "YouTuber"],
+  typeSpeed: 100,
+  BackSpeed: 60,
+  loop: true,
+});
